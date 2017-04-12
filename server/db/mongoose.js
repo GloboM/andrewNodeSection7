b3745427a98c81a6f1fce5d7578ca20128d3fbe6
SUPINFO AppFactory <MiladiMed@MacBookRetina.local>
2017-04-12 14:25:38 +0200

    last changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//mongoose.connect('mongodb://med:med@ds157740.mlab.com:57740/andrewtodo');
-mongoose.connect('mongodb://localhost:27017/TodoAndrew');
+mongoose.connect('mongodb://med:med@ds157740.mlab.com:57740/andrewtodo');
+//mongoose.connect('mongodb://localhost:27017/TodoAndrew');
 
 module.exports = {mongoose}
